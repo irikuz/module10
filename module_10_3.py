703ef23989ce49dd858a71a095025dd8190496c1
irikuz <i-iri.kuz@yandex.ru>
2024-09-08 12:11:04 +0300

    Add files via upload

---
+++ module_10_3.py
@@ -0,0 +1,44 @@
+import threading
+from time import sleep
+import random
+
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+
+        for i in range(100):
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            amount = random.randint(50, 500)
+            self.balance += amount
+            print(f'Пополнение: {amount}. Баланс: {self.balance}')
+            sleep(0.01)
+
+    def take(self):
+        for i in range(100):
+            cash = random.randint(50, 500)
+            print(f'Запрос на вывод {cash}')
+            if cash > self.balance:
+                print(f'Запрос отклонен, недостаточно средств')
+                self.lock.acquire()
+            else:
+                self.balance -= cash
+                print(f'снятие {cash} прошло успешно, баланс {self.balance}')
+
+
+bk = Bank()
+
+# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
